04453c51606057d2add62e6ef46c1a8f4ed4f7f3
ASO2101137 <114889734+ASO201137@users.noreply.github.com>
2023-07-06 21:07:40 +0900

    Add files via upload

--- 04_ソースコード/shinkitouroku.php
+++ 04_ソースコード/shinkitouroku.php
@@ -1,6 +1,9 @@
 <?php
 // エラーメッセージ
 $err = [];
+echo 'test';
+
+
 
 // バリデーション
 if(!$username = filter_input(INPUT_POST, 'username')){
@@ -8,16 +11,19 @@ if(!$username = filter_input(INPUT_POST, 'username')){
 }
 
 if(!$email = filter_input(INPUT_POST, 'email')){
-    $err['mail'] = 'メールの形式が間違っています。';
+    $err['mail'] = 'メールの形式が間違っています。このメールアドレスは既に登録されています。';
 }
 
 $password = filter_input(INPUT_POST, 'password');
 // 正規表現
 if(!preg_match("/\A[a-z\d]{6,16}+\z/i", $password)){
-    $err['pass'] = 'パスワードは６文字から１６文字以下の形式です。
-';
+    $err['pass'] = 'パスワードは６文字から１６文字以下の形式です。パスワードの形式が間違っています。';
 }
 
+
+if(count($err) === 0){
+    //ユーザを登録する処理
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,6 +35,8 @@ if(!preg_match("/\A[a-z\d]{6,16}+\z/i", $password)){
   <link rel="stylesheet" href="css/shinkitouroku.css">
 </head>
 <body>
+<!-- エラーがある場合にforeachで該当エラーを表示させる -->
+  <a class="back-button" href="戻るURL"></a>
   <div class="container">
     <fieldset>
     <h2>新規登録</h2><br>
@@ -38,17 +46,17 @@ if(!preg_match("/\A[a-z\d]{6,16}+\z/i", $password)){
         <label for="username" class="col-form-label">　　ユーザー名：</label>
         <input type="text" class="form-control" id="username" name="username" required>
       </div>
-      <p><?php if (array_key_exists('name', $err)) {echo $err['name']; }?></p>
+      <p><?php if (array_key_exists('name', $err)) {echo $err['name'] }?></p>
       <div class="form-group">
         <label for="email" class="col-form-label">メールアドレス：</label>
         <input type="email" class="form-control" id="email" name="email" required>
       </div>
-      <p><?php if (array_key_exists('mail', $err)) {echo $err['mail']; }?></p>
+      <p><?php if (array_key_exists('mail', $err)) {echo $err['mail'] }?></p>
       <div class="form-group">
         <label for="password" class="col-form-label">　　パスワード：</label>
         <input type="password" class="form-control" id="password" name="password" required>
       </div>
-      <p><?php if (array_key_exists('pass', $err)) {echo $err['pass']; }?></p>
+      <p><?php if (array_key_exists('pass', $err)) {echo $err['pass'] }?></p>
       </fieldset>
       <button type="submit" class="btn btn-primary">登録</button>
     </form>
