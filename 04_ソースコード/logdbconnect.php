bc5d16b66044456a81203bac4af63a444b1c3849
ASO2101155 <114889542+ASO2101155@users.noreply.github.com>
2023-07-21 01:10:53 +0900

    Add files via upload
    CSSが少し未完成だけど、動きは完成

--- 04_ソースコード/logdbconnect.php
+++ 04_ソースコード/logdbconnect.php
@@ -1,8 +1,8 @@
 <?php
 class DBconnect{
     public function ConnectDB($email,$password){
-		$pdo = new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1417856-communication','LAA1417856','communication');
-	$sql = "SELECT * FROM user_data where user_name = ?";
+		$pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417842-communicatio','LAA1417842','communicatio');
+	$sql = "SELECT * FROM user_id where user_name = ?";
 	$ps = $pdo->prepare($sql);
 	$ps->bindValue(1,$email,PDO::PARAM_STR);
         $ps->execute();
@@ -11,7 +11,7 @@ class DBconnect{
     }
     public function insertHash($mail,$pass){
 		session_start();
-        $pdo = new PDO('mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1417856-communication','LAA1417856','communication');
+        $pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417842-communicatio','LAA1417842','communicatio');
         $sql = "INSERT INTO user_data(user_data_id,user_name,password) values(?,?,?)";
         $ps = $pdo->prepare($sql);
 	$rand = mt_rand(1, 999);
@@ -24,30 +24,31 @@ class DBconnect{
         $_SESSION['password'] = $pass;
 	
     }
-    public function outfitSearch($number){
-		$pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417856-communicatio','LAA1417856','communicatio');
-		$sql = "SELECT outfit_img FROM ProductListOutfit where outfit_id = ?";
-		$ps = $pdo->prepare($sql);
-		$ps->bindValue(1,$number,PDO::PARAM_STR);
-        	$ps->execute();
-        	$searchArray = $ps->fetchAll();
-        	return $searchArray;
-     }
-     public function outfitData($number){
-		$pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417856-communicatio','LAA1417856','communicatio');
-		$sql = "SELECT product_data_id,product_name,product_price FROM product_data where outfit_id = ?";	
-		$ps = $pdo->prepare($sql);
-		$ps->bindValue(1,$number,PDO::PARAM_STR);
-        	$ps->execute();
-        	$searchArray = $ps->fetchAll();
-        	return $searchArray;
-     }
 	 public function sessionSearch(){
-		$pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417856-communicatio','LAA1417856','communicatio');
+		$pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417842-communicatio','LAA1417842','communicatio');
 		$sql = "INSERT INTO sample(id,data) values(1,2)";	
 		$ps = $pdo->prepare($sql);
 		$ps->execute();
 	 }
 	  
 }
+class logDBConnect{
+	public function dbConnect(){
+		$pdo = new PDO('mysql:host=mysql214.phy.lolipop.lan;dbname=LAA1417842-communicatio','LAA1417842','communicatio');
+		return $pdo;
+	}
+	public function getUsermail($getmail){
+		$pdo = $this->dbConnect();
+
+		$sql = "SELECT * FROM User_information WHERE mailaddress = ?";
+		$ps = $pdo->prepare($sql);
+		$ps->bindValue(1,$getmail,PDO::PARAM_STR);
+		$ps->execute();
+
+		$a = $ps->fetchAll();
+		return $a;
+
+	}
+}
+
 ?>
---
+++ 04_ソースコード/login.php
@@ -0,0 +1,100 @@
+<?php
+    $email = "";
+    $password = "";
+    $error = array();
+    //DBの情報
+    require_once 'logdbconnect.php';
+    $class = new logDBConnect();
+    // echo $_POST['email']."<br>";
+    $a = $class->getUsermail($_POST['email']);
+    // var_dump($a);
+
+        $error['mail'] = '';
+        $error['password'] = '';
+
+        //POST判定
+        if(!empty($_POST)){
+            $mail = $_POST['email'];
+            $pass = $_POST['password'];
+
+            //空白チェック
+            if ($mail == '') {
+                $error['mail'] = 'メールアドレスが未入力です。';
+            }
+              //メールアドレス形式チェック
+
+              //パスワード空白チェック
+            if ($pass == '') {
+                $error['password'] = 'パスワードが未入力です。';
+            }
+            //
+            if($error['mail'] == "" && $error['password'] == "" ){
+                foreach($a as $row){
+                    if($_POST['email'] == $row['mailaddress']){
+                        $data = $row['pass_word'];
+                    }
+                }
+                if (isset($data) && password_verify($pass, $data)) {
+                    // 認証成功
+                    $_SESSION['mailaddress'] = $mail; // Store the email address in the session
+                    // マイページへ遷移
+                    header('Location: Main.php', true, 307);
+                    exit;
+                } else {
+                    $error['mail'] = 'eメールアドレスまたはパスワードが違います'.$_POST['email'].$data."です";
+                }
+                //if($data == $pass){
+                    //セッションにemailアドレスを挿入する
+                    //$_SESSION['mailaddress'] = $email;
+                    //マイページへ遷移
+                    //header('Location:Main.php');
+                    //exit;
+                //}else{
+                    //$error['mail'] = 'eメールアドレスまたはパスワードが違います';
+                //}
+            }
+
+        }
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./css/style2.css" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width,initial-scale=1"/>
+		<meta charset="UTF-8">
+    </head>
+    <body>
+<form action="" method="post">
+        <!--テキストボックス-->
+        <div class="Main">
+			<div class="a1">
+			<img src="/team2/game.png" width="490" height="570" >
+		</div>
+        <div class="gamelabel">
+			<h1>GAMEコミュニティ</h1>
+		</div>
+        
+        <div class="a2">
+            <div class="flexbox">
+              <input type='email' name='email' placeholder="メールアドレス" >
+            
+            <input type='password' name='password' placeholder="パスワード">
+        </div>
+
+        <div class="err"><?php echo $error['mail']; ?></div>
+        <div class="err"><?php echo $error['password']; ?></div>
+        <div class="logbtn">
+	    
+            <!--ログインボタン-->
+            
+        <div class="button-panel">
+            <button class="btn btn-secondary" type="submit">ログイン</button>
+        </div>
+        <a class="text-dark" href=/team2/entry.php>新規登録はこちら</a>
+            
+        </div>
+    </div>
+</form>
+</html>
