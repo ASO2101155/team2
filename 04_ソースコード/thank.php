b7a907a87764131c50ef868f84dfd1b8a3fafccb
ASO2101155 <114889542+ASO2101155@users.noreply.github.com>
2023-07-21 01:09:36 +0900

    Add files via upload
    前の画面に遷移しないとこ修正したのとログイン画面の遷移も修正した

--- 04_ソースコード/thank.php
+++ 04_ソースコード/thank.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
     <title>会員登録完了</title>
     <link href="https://unpkg.com/sanitize.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/shinki.css">
 </head>
 <body>
     <div class="content">
         <h1>会員登録が完了しました。</h1>
         <p>下のボタンよりログインページに移動してください。</p>
         <br><br>
-        <a href="login.php"><button class="btn">ログインページに移動する</button></a>
+        <a href="/team2/login.php"><button class="btn">ログインページに移動する</button></a>
+	
     </div>
 </body>
 </html>
